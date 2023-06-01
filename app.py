240b6a2b60b24ccc1cb8142beaac50eb0f1a394b
Vidya2000 <130764252+Vidya2000@users.noreply.github.com>
2023-06-01 05:04:43 +0000

    Added to save Query and results to text file

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+import os
+import logging
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(filename="app.log", level=logging.DEBUG)
 
 
 @app.route("/")
 def hello_world():
   return render_template("home.html")
+
+
+@app.route('/search', methods=['POST'])
+def search():
+  keyword = request.form['keyword']
+  with open('search_history.txt', 'a') as file:
+    file.write(keyword + '\n')
+  return render_template('result.html', keyword=keyword)
 
 
 if __name__ == "__main__":
